18003a88325c98be89decf9e2035765fd7841193
Dan Fornika <dfornika@gmail.com>
2021-05-17 15:25:25 -0700

    fix path to micromamba

--- .github/scripts/install_mamba.sh
+++ .github/scripts/install_mamba.sh
@@ -7,6 +7,6 @@ mkdir micromamba && cd micromamba
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 cd ..  & mv micromamba /opt
 
-/opt/micromamba/bin shell init -s bash -p ~/micromamba
+/opt/micromamba/bin/micromamba shell init -s bash -p ~/micromamba
 
 echo 'alias conda=micromamba' >> ~/.bash_aliases
