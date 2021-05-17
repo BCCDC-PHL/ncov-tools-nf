691c8438fb0ddf487d7a52829fb7f08952c06768
Dan Fornika <dfornika@gmail.com>
2021-05-17 15:26:19 -0700

    fix AND operator &&

--- .github/scripts/install_mamba.sh
+++ .github/scripts/install_mamba.sh
@@ -5,7 +5,7 @@ echo "Install Micromamba.." >> artifacts/test_artifact.log
 
 mkdir micromamba && cd micromamba
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-cd ..  & mv micromamba /opt
+cd ..  && mv micromamba /opt
 
 /opt/micromamba/bin/micromamba shell init -s bash -p ~/micromamba
 
