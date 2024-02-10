de5c348cbc43dbaf20a9537607faa458afeb9a6b
Dan Fornika <dfornika@gmail.com>
2024-02-09 17:21:23 -0800

    Update test workflow

--- .github/scripts/create_conda_env.sh
+++ .github/scripts/create_conda_env.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 export PATH=/opt/miniconda3/bin:$PATH
 
-mamba env create --prefix /home/runner/.conda/envs/ncov-qc-40e1fefdb1b312663435a35c6d6cb9da --file /home/runner/work/ncov-tools-nf/ncov-tools-nf/environments/ncov-tools-1.5.yml
+mamba env create --prefix /home/runner/.conda/envs/ncov-qc-86edc7b3dd9d65d41e4044034891dd43 --file /home/runner/work/ncov-tools-nf/ncov-tools-nf/environments/ncov-tools-1.9.yml
