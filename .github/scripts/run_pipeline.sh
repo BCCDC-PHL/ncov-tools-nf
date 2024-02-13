a3f537da1a65729c9a7c71ca207a865d89cd06ed
Dan Fornika <dfornika@gmail.com>
2024-02-12 16:36:58 -0800

    Fix cores

--- .github/scripts/run_pipeline.sh
+++ .github/scripts/run_pipeline.sh
@@ -7,9 +7,9 @@ export PATH=/opt/miniconda3/bin:$PATH
 BCCDC_NCOV2019_ARTIC_PIPELINE_VERSION="1.3.4"
 
 # the github runner only has 4 cpus available
-#sed -i s'/cpus = 14/cpus = 4/'g nextflow.config
-#sed -i s'/--cores 14/--cores 4/'g modules/ncov-tools.nf
-#sed -i s'/--cores 8/--cores 4/'g modules/ncov-tools.nf
+sed -i s'/cpus = 14/cpus = 4/'g nextflow.config
+sed -i s'/--cores 14/--cores 4/'g modules/ncov-tools.nf
+sed -i s'/--cores 8/--cores 4/'g modules/ncov-tools.nf
 
 nextflow run main.nf \
 	 -profile conda \
